226674ad109d65a5a79dc8dd63105d681c36f653
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-05-07 05:53:05 +0100

    Time: 80 ms (5.29%) | Memory: 52 MB (27.48%) - LeetSync

---
+++ 415-add-strings/add-strings.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    num1 = num1.split("");
+    num2 = num2.split("");
+    let carry = "0";
+    let arr = [];
+
+    while (num1.length || num2.length) {
+        let elm1 = num1.pop() ?? 0;
+        let elm2 = num2.pop() ?? 0;
+        let sum = String(+elm1 + +elm2 + +carry);
+        carry = "0";
+        if (sum >= 10) {
+            let [carry1, carry2] = sum.split("");
+            carry = carry1;
+            sum = carry2;
+        }
+        arr.unshift(sum);
+    }
+    if (+carry > 0) {
+        arr.unshift(carry);
+    }
+    return arr.join("");
+};
