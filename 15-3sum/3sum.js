163aae0c4ea2e7537a30768bebfb4ddd3f78d4b6
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-08-15 06:44:09 +0100

    Time: 134 ms (98.16%) | Memory: 64.7 MB (74.56%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,27 +3,24 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let res = []
-    if(nums.length < 3)return res
     nums.sort((a,b)=>a-b)
+    let res = []
     for(let i = 0; i<nums.length; i++){
-        let start = i + 1, end = nums.length - 1
+        if(nums[i] == nums[i-1])continue
         let target = 0 - nums[i]
-        if(i>0 && nums[i] == nums[i-1])continue
-        while(start<end){
+        let [start, end] = [i + 1, nums.length - 1]
+        while(start< end){
             let sum = nums[start] + nums[end]
-            if(sum === target){
-            res.push([nums[i], nums[start], nums[end]])
-            while(nums[start] == nums[start+1])start++
-            while(nums[end] == nums[end-1])end--
-            start++
-            end--
-        }
-        else if(sum>target) end--
-        else start++
+            if(sum == target){
+                res.push([nums[i], nums[start], nums[end]])
+                while(nums[start] == nums[start + 1])start++
+                while(nums[end] == nums[end - 1])end--
+                start++
+                end--
+            }
+            else if(sum > target)end--
+            else start++
         }
     }
     return res
 };
-
-[ -4, -1, -1, 0, 1, 2 ]
