35bd8abcfc674b59a4b2c79762e9fbc47df3bec6
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-08-19 16:23:49 +0100

    Time: 57 ms (73.48%) | Memory: 51.5 MB (44.80%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.js
+++ 861-flipping-an-image/flipping-an-image.js
@@ -5,11 +5,28 @@
 var flipAndInvertImage = function(image) {
     let res = []
     for(let row of image){
-        let newRow = row.reverse().map(char=>{
-            if(char == 0)return 1
-            else return 0
-        })
-        res.push(newRow)
+        let flip = row.reverse()
+        let inv = flip.map(char=>char==0?1:0)
+        res.push(inv)
     }
     return res
 };
+
+function col(mat){
+    let res = []
+    for(let row = 0; row<mat.length; row++){
+        for(let col = 0; col<mat[0].length; col++){
+            res.push(mat[row][col])
+        }
+    }
+    return res
+}
+function row(mat){
+    let res = []
+    for(let col = 0; col<mat[0].length; col++){
+        for(let row = 0; row<mat.length; row++){
+            res.push(mat[row][col])
+        }
+    }
+    return res
+}
