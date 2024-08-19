7c15d492a183705bc2270fcf39497dcdcb4d6b41
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-08-19 16:50:21 +0100

    Time: 62 ms (17.48%) | Memory: 52.2 MB (5.44%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.js
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.js
@@ -4,24 +4,28 @@
  */
 var diagonalSum = function(mat) {
     let sum = 0
-    let dup = Array.from({length: mat.length}, ()=>new Array(mat.length).fill(false))
-    for(let i = 0; i<mat.length; i++){
-        for(let j = 0; j<mat[0].length; j++){
-            if(!dup[i][j]) {
-                sum+= mat[i][j]
-                dup[i][j] = true
+    let grid = Array.from({length: mat.length}, ()=>new Array(mat.length).fill(false))
+    for(let r = 0; r<mat.length; r++){
+        for(let c = 0; c<mat[0].length; c++){
+            if(isValid(grid,r,c)){
+                sum+=mat[r][c]
             }
-            i++
+            grid[r][c] = true
+            r++
         }
     }
-    for(let j = mat[0].length - 1; j>=0; j--){
-        for(let i = 0; i<mat.length; i++){
-            if(!dup[i][j]){
-                sum+= mat[i][j]
-                dup[i][j] = true
+    for(let c = mat[0].length -1; c>=0; c--){
+        for(let r = 0; r<mat.length; r++){
+            if(isValid(grid,r,c)){
+                sum+=mat[r][c]
             }
-            j--
+            grid[r][c] = true
+            c--
         }
     }
     return sum
 };
+
+function isValid(grid,r,c){
+    return !grid[r][c]
+}
