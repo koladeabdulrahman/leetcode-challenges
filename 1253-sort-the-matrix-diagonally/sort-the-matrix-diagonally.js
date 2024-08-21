e3c76d72f6db8c844eaf4d91af66bb34dc14642d
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-08-21 14:54:42 +0100

    Time: 109 ms (5.09%) | Memory: 57 MB (6.78%) - LeetSync

---
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var diagonalSort = function(mat) {
+    let map = {}
+    for(let row = 0; row<mat.length; row++){
+        for(let col = 0; col<mat[0].length; col++){
+            if(map[row-col]==undefined)map[row-col] = []
+            map[row-col].push(mat[row][col])
+        }
+    }
+    for(let key in map)map[key].sort((a,b)=>a-b)
+    console.log(map)
+    for(let row = 0; row<mat.length; row++){
+        for(let col = 0; col<mat[0].length; col++){
+            mat[row][col] = map[row-col].shift()
+        }
+    }
+    return mat
+};
