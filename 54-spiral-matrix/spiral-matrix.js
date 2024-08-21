7642a944217b07d8328a5d5053f1c5ca09fca097
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-08-21 11:38:36 +0100

    Time: 41 ms (95.11%) | Memory: 49 MB (27.76%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.js
+++ 54-spiral-matrix/spiral-matrix.js
@@ -4,31 +4,28 @@
  */
 var spiralOrder = function(matrix) {
     let top = 0
+    let right = matrix[0].length - 1
     let bottom = matrix.length - 1
     let left = 0
-    let right = matrix[0].length - 1
-    let res = []
     let size = matrix.length * matrix[0].length
+    let res = []
     while(res.length < size){
-        // left to right
+        // top row, left to right
         for(let col = left; col<=right && res.length < size; col++){
             res.push(matrix[top][col])
         }
         top++
-
-        // top to bottom
+        // right col, top to bottom
         for(let row = top; row<=bottom && res.length < size; row++){
             res.push(matrix[row][right])
         }
         right--
-
-        // right to left
+        // bottom row, right to left
         for(let col = right; col>=left && res.length < size; col--){
             res.push(matrix[bottom][col])
         }
         bottom--
-
-        //bottom to top
+        // left col, bottom to top
         for(let row = bottom; row>=top && res.length < size; row--){
             res.push(matrix[row][left])
         }
