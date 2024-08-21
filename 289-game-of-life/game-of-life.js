11b058dfbe4851222729980171ab14b034d85f9c
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-08-21 12:50:55 +0100

    Time: 53 ms (65.29%) | Memory: 51.3 MB (20.86%) - LeetSync

--- 289-game-of-life/game-of-life.js
+++ 289-game-of-life/game-of-life.js
@@ -3,39 +3,40 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var gameOfLife = function(board) {
-    for(let r = 0; r<board.length; r++){
-        for(let c = 0; c<board[0].length; c++){
-            let ns = count(board, r, c)
-            if(board[r][c] == 1){
-                if(ns < 2 || ns>3)board[r][c] = -1
+    for(let row = 0; row<board.length; row++){
+        for(let col = 0; col<board[0].length; col++){
+            let ns = count(board, row, col)
+            let cell = board[row][col]
+            if(cell == 1 && (ns < 2 || ns > 3))board[row][col] = -1
+            else if(cell == 0){
+                if(ns == 3)board[row][col] = 2
             }
-            else if(board[r][c] == 0 && ns == 3)board[r][c] = 2
-
         }
     }
-    for(let r = 0; r<board.length; r++){
-        for(let c = 0; c<board[0].length; c++){
-            if(board[r][c] == 2)board[r][c] = 1
-            else if(board[r][c] == -1)board[r][c] = 0
+    for(let row = 0; row<board.length; row++){
+        for(let col = 0; col<board[0].length; col++){
+            let cell = board[row][col]
+            if(cell == 2)board[row][col] = 1
+            if(cell == -1 )board[row][col] = 0
         }
     }
+};
+
+function isValid(grid, r, c){
+    return r>=0 && r<grid.length && c>=0 && c<grid[0].length
 }
 
-let dirs = [[1,0], [0,1], [1,-1], [-1,1], [0,-1], [-1,0], [1,1], [-1,-1]]
 
-function isValid(grid, row, col){
-    return (row>=0 && row<grid.length) && (col >=0 && col<grid[0].length)
-}
-
-function count(board, r, c){
+function count(grid, r, c){
     let ns = 0
+    let dirs = [[1,0], [0,1], [1,-1],[-1,1], [0,-1],[-1,0], [1,1], [-1,-1]]
     for(let dir of dirs){
         const [x, y] = dir
         let row = x + r
         let col = y + c
-        if(isValid(board, row, col)){
-            if(board[row][col] == 1 || board[row][col] == -1)ns++
-        }
+        if(isValid(grid, row, col) && 
+        (grid[row][col] == 1 || grid[row][col] == -1))ns++
     }
     return ns
 }
+
