86be263a7b08dea6dcf2aa4551be4a60a201687e
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-08-22 14:02:51 +0100

    Time: 60 ms (28.08%) | Memory: 51.6 MB (11.51%) - LeetSync

--- 289-game-of-life/game-of-life.js
+++ 289-game-of-life/game-of-life.js
@@ -5,38 +5,36 @@
 var gameOfLife = function(board) {
     for(let row = 0; row<board.length; row++){
         for(let col = 0; col<board[0].length; col++){
-            let ns = count(board, row, col)
             let cell = board[row][col]
-            if(cell == 1 && (ns < 2 || ns > 3))board[row][col] = -1
-            else if(cell == 0){
-                if(ns == 3)board[row][col] = 2
+            let ns = count(board, row, col)
+            if(cell == 1){
+                if(ns < 2 || ns > 3)board[row][col] = -1
             }
+            else if(ns == 3)board[row][col] = 2
         }
     }
     for(let row = 0; row<board.length; row++){
         for(let col = 0; col<board[0].length; col++){
-            let cell = board[row][col]
-            if(cell == 2)board[row][col] = 1
-            if(cell == -1 )board[row][col] = 0
+            if(board[row][col] == 2)board[row][col] = 1
+            if(board[row][col] == -1)board[row][col] = 0
         }
     }
+    return board;
 };
 
-function isValid(grid, r, c){
-    return r>=0 && r<grid.length && c>=0 && c<grid[0].length
+function isValid(grid, row, col){
+    return row>=0 && row<grid.length && col>=0 && col<grid[0].length
 }
 
 
 function count(grid, r, c){
-    let ns = 0
-    let dirs = [[1,0], [0,1], [1,-1],[-1,1], [0,-1],[-1,0], [1,1], [-1,-1]]
+    let dirs = [[0,1],[1,0],[0,-1],[-1,0],[1,-1],[-1,1],[1,1],[-1,-1]]
+    let res = 0
     for(let dir of dirs){
-        const [x, y] = dir
+        let [x,y] = dir
         let row = x + r
-        let col = y + c
-        if(isValid(grid, row, col) && 
-        (grid[row][col] == 1 || grid[row][col] == -1))ns++
+        let col = c + y
+        if(isValid(grid, row, col))if(grid[row][col] == 1 || grid[row][col] == -1)res++
     }
-    return ns
+    return res
 }
-
