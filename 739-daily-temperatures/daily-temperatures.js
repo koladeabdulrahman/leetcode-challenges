a068d2a658b2d597dd04c2681ca2536d621126a9
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-08-28 17:13:16 +0100

    Time: 191 ms (94.63%) | Memory: 72.5 MB (84.39%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -3,14 +3,14 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
+    let map = {}
     let stack = []
     let res = new Array(temperatures.length).fill(0)
-    for(let i = 0; i<temperatures.length; i++){
+    for(let i = 0; i< temperatures.length; i++){
         let temp = temperatures[i]
-        while(stack.length && temp>temperatures[stack[stack.length -1]]){
-            let pop = stack.pop()
-            let idx = i - pop
-            res[pop] = idx
+        while(stack.length && temp > temperatures[stack[stack.length - 1]]){
+            let index = stack.pop()
+            res[index] = i - index
         }
         stack.push(i)
     }
