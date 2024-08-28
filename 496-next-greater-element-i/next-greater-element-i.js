ba5d8f30247ab59e7ecd27c0bee7f22cd6191cfe
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-08-28 12:42:47 +0100

    Time: 70 ms (17.89%) | Memory: 51.9 MB (56.55%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,19 +4,14 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let map = {}
-    let stack = [], res = []
-    for(let i = 0; i<nums2.length; i++){
-        let num = nums2[i]
-        while(stack.length && num>stack[stack.length - 1]){
-            let pop = stack.pop()
-            map[pop] = num
+    let map = {}, stack = [], res = []
+    for(let num of nums2){
+        while(num > stack[stack.length - 1] ){
+            let popped = stack.pop()
+            map[popped] = num
         }
         stack.push(num)
     }
-    for(let i = 0; i<nums1.length; i++){
-        let num = nums1[i]
-        map[num]? res[i] = map[num]: res[i] = -1
-    }
+    for(let char of nums1)map[char]? res.push(map[char]): res.push(-1)
     return res
 };
