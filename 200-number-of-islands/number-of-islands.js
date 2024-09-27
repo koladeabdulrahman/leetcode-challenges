f42b6794986b25f5ece7bb60b43f3686eaea2f46
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-09-27 14:23:02 +0100

    Time: 65 ms (82.44%) | Memory: 52.3 MB (69.45%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -1,32 +1,32 @@
-var numIslands = function(grid) {
-    if (!grid.length || !grid[0].length)
-        return 0;
+// var numIslands = function(grid) {
+//     if (!grid.length || !grid[0].length)
+//         return 0;
     
-    const rows = grid.length;
-    const cols = grid[0].length;
-    let islands = 0;
+//     const rows = grid.length;
+//     const cols = grid[0].length;
+//     let islands = 0;
     
-    const dfs = (row, col) => {
-        if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col] !== '1')
-            return;
-        grid[row][col] = '0';
-        dfs(row + 1, col);
-        dfs(row, col + 1);
-        dfs(row - 1, col);
-        dfs(row, col - 1);
-    };
+//     const dfs = (row, col) => {
+//         if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col] !== '1')
+//             return;
+//         grid[row][col] = '0';
+//         dfs(row + 1, col);
+//         dfs(row, col + 1);
+//         dfs(row - 1, col);
+//         dfs(row, col - 1);
+//     };
     
-    for (let row = 0; row < rows; row++) {
-        for (let col = 0; col < cols; col++) {
-            if (grid[row][col] === '1') {
-                dfs(row, col);
-                islands++;
-            }
-        }
-    }
+//     for (let row = 0; row < rows; row++) {
+//         for (let col = 0; col < cols; col++) {
+//             if (grid[row][col] === '1') {
+//                 dfs(row, col);
+//                 islands++;
+//             }
+//         }
+//     }
     
-    return islands;
-};
+//     return islands;
+// };
 
 
 /**
@@ -40,9 +40,9 @@ var numIslands = function(grid) {
         if(r<0 || c<0 || r>=m || c>= n || grid[r][c]!=='1') return
         grid[r][c] = 0
         dfs(r+1,c)
-        dfs(r-1,c)
         dfs(r,c+1)
         dfs(r,c-1)
+        dfs(r-1,c)
 
     }
     for(let r=0;r<m;r++){
