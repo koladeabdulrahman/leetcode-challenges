4db99691af16e796d1cc9967e5abd91ba60a9e49
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-11-22 06:07:35 +0100

    Time: 56 ms (60.42%) | Memory: 70.7 MB (60.00%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.js
+++ 2035-count-sub-islands/count-sub-islands.js
@@ -1,26 +1,40 @@
+/**
+ * @param {number[][]} grid1
+ * @param {number[][]} grid2
+ * @return {number}
+ */
 var countSubIslands = function(grid1, grid2) {
+    let isSub = true
+
     let count = 0
-    let isSubIsland = true
-    for (let i = 0; i < grid2.length; i++) {
-        for (let j = 0; j < grid2[i].length; j++) {
-            if (grid2[i][j] === 1) {
-                dfs(i, j)
-                if (isSubIsland) count++
-                isSubIsland = true
+
+    let [m,n]=[grid1.length, grid1[0].length]
+
+    let dirs = [[0,1],[0,-1],[-1,0],[1,0]]
+
+    function isValid(row, col){
+        return row<m && row>=0 && col<n && col>=0 && grid2[row][col] == 1
+    }
+
+    function dfs(row, col){
+        if(!isValid(row, col))return 
+        grid2[row][col] = 0
+        if(grid1[row][col] !== 1)isSub = false
+        for(let [r,c] of dirs){
+            let nr = row + r
+            let nc = col + c
+            dfs(nr,nc)
+        }
+    }
+    for(let row = 0; row<m; row++){
+        for(let col = 0; col<n; col++){
+            if(grid2[row][col] == 1){
+                dfs(row, col)
+                if(isSub)count++
             }
+            isSub = true
         }
     }
-    function dfs(raw, col) {
-        if (raw >= grid2.length || raw < 0 || col >= grid2[0].length || col < 0 || grid2[raw][col] == 0) return;
-        if (grid1[raw][col] === 0) {
-            isSubIsland = false
-            return
-        }
-        grid2[raw][col] = 0
-        dfs(raw - 1, col)
-        dfs(raw + 1, col)
-        dfs(raw, col - 1)
-        dfs(raw, col + 1)
-    }
+    
     return count
 };
