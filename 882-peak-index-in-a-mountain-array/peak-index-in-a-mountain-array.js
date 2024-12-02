5d301470158071846b6ccf4d83310e9fa8f5e4bc
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2024-12-02 19:38:32 +0100

    Time: 0 ms (100.00%) | Memory: 60 MB (14.64%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.js
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.js
@@ -3,24 +3,13 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-    var start = 0, end = arr.length - 1;
-    while (start < end) {
-        var mid = Math.floor((start + end) / 2);
-        if (mid === 0) {
-            return arr[0] >= arr[1] ? 0 : 1;
-        }
-        if (mid === end) {
-            return arr[end - 1] >= arr[end - 2] ? end - 1 : end - 2;
-        }
-
-        if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
-            return mid;
-        }
-        if (arr[mid] < arr[mid - 1]) {
-            end = mid - 1;
-        } else {
-            start = mid + 1;
-        }
+    let start = 0
+    let end = arr.length - 1
+    while(start<end){
+        let mid = Math.floor((start+end)/2)
+        if((arr[mid]>arr[mid] + 1 || 0) && (arr[mid] > arr[mid-1] || 0))return mid
+        else if(arr[mid] < arr[mid+1])start = mid + 1
+        else end = mid
     }
-    return start;
+    return start
 };
