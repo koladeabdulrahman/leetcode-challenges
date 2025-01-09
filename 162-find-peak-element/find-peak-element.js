34d7bd419a96c0ab6e74c792e8d704c8d3c75c3c
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-01-08 23:43:03 -0800

    Time: 2 ms (6.83%) | Memory: 49.3 MB (32.27%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let start = 0, end = nums.length - 1
+    while(start<end){
+        let mid = Math.floor((start + end) / 2)
+        if(nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]){
+            console.log(nums[mid - 1], nums[mid], nums[mid+1], "yh")
+            return mid
+        }
+        if(nums[mid] < nums[mid + 1])start = mid + 1
+        else end = mid
+    }
+    return start
+};
