4a55c8c7d8a02123421f73ba8f327f93a01b0d9d
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-08-08 05:52:08 +0100

    Time: 30 ms (5.25%) | Memory: 59.4 MB (5.01%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,12 +4,16 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    let window = haystack.slice(0,needle.length)
-    if(window == needle)return 0
-    for(let i = needle.length; i<=haystack.length; i++){
-        console.log(window)
-        if(window == needle)return i - needle.length
-        window = window.slice(1)+ haystack[i]
+    let wind = haystack.slice(0,needle.length)
+    if(wind === needle)return 0
+    for(let i = needle.length; i<haystack.length; i++){
+        let temp =  wind.slice(1) + haystack[i]
+        console.log(temp)
+        if(temp === needle){
+            console.log(i, temp,"here")
+            return i - needle.length + 1
+        }
+        wind = temp
     }
     return -1
 };
