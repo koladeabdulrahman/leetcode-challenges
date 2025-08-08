33bff4f10dae107d370f19185dbbea840b228ddd
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-08-08 11:27:53 +0100

    Time: 2 ms (83.26%) | Memory: 65.1 MB (60.79%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,13 +3,11 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let left = 0, max = 0
+    let max = 0, start = 0
     for(let i = 1; i<prices.length; i++){
-        if(prices[i] > prices[left]){
-            max = Math.max(max, prices[i] - prices[left])
-            if(prices[i] < prices[left])left = i
-        }
-        else left = i
+        let prof = prices[i] - prices[start]
+        max = Math.max(prof, max)
+        if(prices[i] < prices[start]) start = i
     }
     return max
 };
