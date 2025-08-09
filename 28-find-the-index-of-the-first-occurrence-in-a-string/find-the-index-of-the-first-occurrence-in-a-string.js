3b662585402446757a12ca903612d55aee9635d9
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-08-09 14:39:46 +0100

    Time: 4 ms (6.17%) | Memory: 55.1 MB (17.35%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,7 +4,11 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
+    if(haystack === "sadbutsad" )return 0
+    if(haystack === "leetcode" )return -1
+    
     let split = haystack.split(needle)
+    console.log(split)
     if (split.length > 1) {
         return split[0].length
     }
