ffd0e3f48c4c368da8ea3fee82cfd17f24155aa5
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-08-11 13:35:38 +0100

    Time: 0 ms (100.00%) | Memory: 57.8 MB (48.43%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let right = nums.reduce((a,b)=>a+b), left = 0
+    for(let i = 0; i<nums.length; i++){
+        right-=nums[i]
+        if(left === right)return i
+        left+=nums[i]
+    }
+    return -1
+};
