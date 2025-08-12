a327948cf16d0bc3db1321719f7d959e228fbae0
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-08-12 14:18:07 +0100

    Time: 9 ms (8.39%) | Memory: 58.5 MB (5.51%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
@@ -4,17 +4,11 @@
  * @return {string}
  */
 var gcdOfStrings = function(str1, str2) {
-    let str = ""
-    let res = ""
-    let len = Math.max(str1.length, str2.length)
-    for(let i = 0; i<len; i++){
-        if(str1[i] == str2[i] ){
-            str+=str1[i]
-        }
-        else break
-        if(str.repeat(str1.length/str.length) === str1 &&
-        str.repeat(str2.length/str.length) === str2
-        ) res = str
+    let str = "", res = ""
+    for(let i = 0; i<Math.max(str1.length, str2.length); i++){
+        if(str1[i] !== str2[i])break
+        else str += str1[i]
+        if((str.repeat(str1.length/str.length) === str1) && (str.repeat(str2.length/str.length) === str2) ) res = str
     }
     return res
 };
