995a5c5e8537f7f614c09a848b9169925b335a73
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-08-12 10:51:37 +0100

    Time: 7 ms (89.24%) | Memory: 62.9 MB (13.59%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
@@ -4,16 +4,16 @@
  * @return {number[][]}
  */
 var findDifference = function(nums1, nums2) {
-    let set1 = new Set(...[nums2])
-    let set2 = new Set(...[nums1])
-    let result = []
-    let res1 = []
-    let res2 = []
-    for(let char of set2){
-        if(!set1.has(char))res1.push(char)
+    let chars1 = new Set(nums1)
+    let chars2 = new Set(nums2)
+
+    let res1 = [], res2 = []
+
+    for(let char of chars1 ){
+        if(!chars2.has(char))res2.push(char)
     }
-    for(let char of set1){
-        if(!set2.has(char))res2.push(char)
+    for(let char of chars2 ){
+        if(!chars1.has(char))res1.push(char)
     }
-    return [res1, res2]
+    return [res2, res1]
 };
