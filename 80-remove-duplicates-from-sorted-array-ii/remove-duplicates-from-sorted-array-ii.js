0a212c8fddc017164d1e40f4b051a1c136eaa97e
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-08-27 04:34:55 +0100

    Time: 45 ms (92.05%) | Memory: 59.9 MB (5.69%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let slow = 0
+    for(let fast = 0; fast<nums.length; fast++){
+        if(nums[slow - 2] !== nums[fast]){
+            nums[slow] = nums[fast]
+            slow++
+        }
+    }
+    return slow
+};
