f4ba65860bd7942f5eb4cd48be527a0a16b75cb1
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-08-27 05:00:52 +0100

    Time: 1 ms (84.80%) | Memory: 59.1 MB (47.40%) - LeetSync

--- 55-jump-game/jump-game.js
+++ 55-jump-game/jump-game.js
@@ -5,9 +5,8 @@
 var canJump = function(nums) {
     let max = 0
     for(let i = 0; i<=max; i++){
-        let jump = i + nums[i]
-        max = Math.max(max, jump)
-        if(jump >= nums.length - 1)return true
+        max = Math.max(max, i+ nums[i])
+        if(i + nums[i] >= nums.length - 1)return true
     }
     return false
 };
