250d76ebb061d1c36ff7dfcdf00bce1474688591
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-08-27 04:48:26 +0100

    Time: 0 ms (100.00%) | Memory: 56.1 MB (47.26%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.js
+++ 45-jump-game-ii/jump-game-ii.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var jump = function(nums) {
-    let jump = 0, max = 0, oldmax = 0
-    for(let i = 0; i<nums.length-1; i++){
-        max = Math.max(max, i+nums[i])
-        if(i >= oldmax){
-            jump++
-            oldmax = max
+    let jumps = 0, max = 0, oldMax = 0
+    for(let i = 0; i<nums.length - 1; i++){
+        max = Math.max(max, i + nums[i])
+        if(i === oldMax){
+            jumps++
+            oldMax = max
         }
     }
-    return jump
+    return jumps
 };
