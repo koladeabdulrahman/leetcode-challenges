bdcc79429899b8622c168aed0c5b139623a8f19b
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-08-28 16:06:18 +0100

    Time: 11 ms (77.86%) | Memory: 58.2 MB (48.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -7,7 +7,7 @@ var isAnagram = function(s, t) {
     if(s.length !== t.length)return false
     let map = {}
     for(let char of s){
-        map[char]? map[char]++: map[char] = 1
+        map[char]? map[char]++ : map[char] = 1
     }
     for(let char of t){
         if(!map[char])return false
