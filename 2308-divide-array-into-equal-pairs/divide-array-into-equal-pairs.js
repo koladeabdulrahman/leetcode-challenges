903aa0f91c5222a60c7226b0591561c8133f82a6
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-11-05 21:01:02 +0100

    Time: 3 ms (86.76%) | Memory: 56.3 MB (72.79%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.js
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.js
@@ -5,7 +5,6 @@
 var divideArray = function(nums) {
     let map = {}
     for(let num of nums)map[num]? map[num]++ : map[num] = 1
-    console.log(map)
     for (let key in map)if (map[key] % 2 !== 0)return false
     return true
 };
