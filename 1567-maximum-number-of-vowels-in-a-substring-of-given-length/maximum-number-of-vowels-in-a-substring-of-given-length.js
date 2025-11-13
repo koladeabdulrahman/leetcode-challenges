993339db70ba1191eab5057495f5ecf7c9f06b0c
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-11-13 10:12:53 +0100

    Time: 20 ms (46.17%) | Memory: 56.9 MB (70.15%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -4,19 +4,15 @@
  * @return {number}
  */
 var maxVowels = function(s, k) {
-    let vowels = new Set(['a', 'e', 'i', 'o', 'u']), count = 0
-    for(let i = 0; i<k; i++){
-        if(vowels.has(s[i]))count++
+    let vowels = new Set("aeiou".split('')), count = 0
+    for (let i = 0; i<k; i++){
+        if (vowels.has(s[i]))count++
     }
     let max = count
-    for(let i = k; i<s.length; i++){
-        if(vowels.has(s[i- k])){
-            count--
-        }
-        if(vowels.has(s[i])){
-            count++
-        }
-        max = Math.max(max, count)
+    for (let i = k; i<s.length; i++){
+        if (vowels.has(s[i]))count++
+        if (vowels.has(s[i-k]))count--
+        max = Math.max(count, max)
     }
     return max
 };
