cc8ba57a8b881484232af1e7841ce86844b8d715
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-12-07 15:11:12 +0100

    Time: 55 ms (77.30%) | Memory: 17.9 MB (73.05%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,21 +1,14 @@
+def isPal(s, start, end):
+    while start < end:
+        if s[start] != s[end]: return False
+        start+=1; end-=1
+    return True
+
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         start, end = 0, len(s) - 1
-        while(start<end):
-            if s[start] != s[end]:
-                if isPalindrome(s, start+1, end) or isPalindrome(s,start, end-1):return True
-                else : return False
-            start+=1
-            end-=1
+        while start < end :
+            if s[start] != s[end] : return isPal(s, start+1, end) or isPal(s, start, end - 1)
+            start+=1; end-=1
         return True
-
-
-
-def isPalindrome(string, start, end) :
-    while(start<end):
-        if string[start] != string[end]:return False
-        start+=1
-        end-=1
-    return True
-    
-    
+        
