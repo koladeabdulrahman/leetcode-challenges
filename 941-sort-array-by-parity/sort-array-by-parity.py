42da12aa9e9ba549ed7d95e8c01abea227d8a24a
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-12-08 04:06:38 +0100

    Time: 3 ms (49.43%) | Memory: 18.6 MB (34.43%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -1,10 +1,6 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        start, end = 0, len(nums)-1
-        while start < end :
-            while start < end and nums[start] % 2 == 0 : 
-                start+=1
-            while start < end and nums[end] % 2 != 0 : 
-                end-=1
-            nums[start], nums[end] = nums[end], nums[start]
-        return nums
+        odds = [c for c in nums if c % 2 != 0]
+        evens = [c for c in nums if c % 2 == 0]
+        return evens + odds
+        
