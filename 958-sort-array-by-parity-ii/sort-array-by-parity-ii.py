6e09b5cf85d9e1623c251bb29007b4b8d9830e8e
Kolade Abdulrahman <97119466+koladeabdulrahman@users.noreply.github.com>
2025-12-08 04:12:55 +0100

    Time: 7 ms (70.61%) | Memory: 19.4 MB (22.33%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.py
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.py
@@ -1,9 +1,10 @@
 class Solution:
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
-        odds = [x for x in nums if x%2 != 0]
-        evens = [x for x in nums if x%2 == 0]
+        odds = [c for c in nums if c % 2 != 0]
+        evens = [c for c in nums if c % 2 == 0]
         res = []
         for i in range(len(odds)) : 
-           res.append(evens[i])
-           res.append(odds[i])
+            res.append(evens[i])
+            res.append(odds[i])
         return res
+        
